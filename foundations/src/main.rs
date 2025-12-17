c6d221eb5fcc946d17915e81ca83b80bf871dc97
ShigekuniWork <shigekuni.work@gmail.com>
2025-12-18 08:34:43 +0900

    number and bool

--- foundations/src/main.rs
+++ foundations/src/main.rs
@@ -1,4 +1,15 @@
 fn main() {
+    // number
+    assert_eq!(2_i32.pow(4), 16);
+    assert_eq!((-4_i32).abs(), 4);
+    assert_eq!(0b101101_u8.count_ones(), 4);
+    
+    assert_eq!(2_i32.checked_pow(4), Some(16));
+    assert_eq!(100_u8.checked_add(200), None);
+    assert_eq!((-4_i32).checked_abs(), Some(4));
+    assert_eq!((-128_i8).checked_div(-1), None);
+    
+    // bool
     assert_eq!(false as i32, 0);
     assert_eq!(true as i32, 1);
 }
