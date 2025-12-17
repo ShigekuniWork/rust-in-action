b00d138cb1b69227f62d1178dfc5da832825f291
ShigekuniWork <shigekuni.work@gmail.com>
2025-12-18 07:45:27 +0900

    over flow

--- foundations/src/main.rs
+++ foundations/src/main.rs
@@ -1,14 +1,23 @@
 fn main() {
-    let a = 10;
-    let b: i32 = 20;
-    let c = 30i32;
-    let d = 30_i32;
+    let twenty = 10;
+    let twenty_one: i32 = 21;
+    let twenty_two = 22i32;
 
-    let total = add(add(a, b), add(c, d));
+    let addition = twenty + twenty_one + twenty_two;
+    println!(
+        "{} + {} + {} = {}",
+        twenty, twenty_one, twenty_two, addition
+    );
 
-    println!("(a + b) + (c + d) = {}", total)
-}
+    let one_million: i64 = 1_000_001;
+    println!("{}", one_million.pow(2));
 
-fn add(a: i32, b: i32) -> i32 {
-    a + b
+    let total = addition + one_million.pow(2) as i32;
+    // i64からi32に縮めたことによって、下位32bitだけが残される。
+    // この場合は、32bit目が1で入れ替わってしまうため、-として答えが出る。
+    println!("{}", total);
+
+    let forty_twos = [42.0, 42f32, 42.0_f32];
+
+    println!("{}", forty_twos[0]);
 }
